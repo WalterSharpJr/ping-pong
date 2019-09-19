3381c2aa06d491bb66b4f1f075eeacb3f2d6f745
Walter Sharp <wsharpjnr@gmail.com>
2019-09-19 21:32:56 +0200

    Fixed issue with enter key submitting the form inside the search box component

--- PingPong.Web/src/components/Filtering/Searchbox.tsx
+++ PingPong.Web/src/components/Filtering/Searchbox.tsx
@@ -8,9 +8,9 @@ export default class SearchBox extends React.Component<{ placeholder?: string, o
 	render()
 	{
 		return (
-			<form className="d-sm-inline-block form-inline my-md-0 mw-100">
+			<form  className="d-sm-inline-block form-inline my-md-0 mw-100">
 				<div className="input-group">
-					<input onKeyPress={ (e) => { if(e.keyCode === 13) this.props.onSearchClicked();  } } onChange={ (e) => { this.props.onSearchChanged(e.target.value) }} type="text" className="form-control bg-light border-0 small" placeholder={ this.props.placeholder } />
+					<input onKeyDown={ (e) => { if(e.keyCode === 13) { this.props.onSearchClicked(); e.preventDefault(); return false; } } } onChange={ (e) => { this.props.onSearchChanged(e.target.value) }} type="text" className="form-control bg-light border-0 small" placeholder={ this.props.placeholder } />
 					<div className="input-group-append">
 						<button className="btn btn-primary" type="button" onClick={ (e) => { this.props.onSearchClicked() } }>
 							<FontAwesomeIcon icon={ faSearch } ></FontAwesomeIcon>                  						
